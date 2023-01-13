b996704890f14644e8f66e79ef2a2edd09f1f735
MrMussati <bruno.cesar.rocha12@gmail.com>
2023-01-13 02:14:25 +0000

    python

--- Traning/GAME_CARD_BUSTERS.py
+++ Traning/GAME_CARD_BUSTERS.py
@@ -1,51 +1,23 @@
-
-#cards_player_one = 10,6,8,9,7,12,7 
-
-#cards_player_one = 7,6,9,5,2,8,11
-
-#Seven rounds 
-
-#Rounds are compared to see the highest valued card
-
-#The player with the highest value card wins the round
-
-#If both cards have the same value it is a tie
-
-#The determine which player has won the game OVERALL or if the game has ended TIE
-
-#should process the cards in the order that they have been supplied in the Lists
-
-#One loop can handle all the game - #Elements in the list
-
-#The variable called round should be charged *FUNCTION
-
 print('******* Card Busters *******')
 
-player_1 = [10,6,8,9,7,12,7]
-player_2 = [7,6,9,5,2,8,11]
 
-i = player_1,player_2
+player_1 = [10, 6, 8, 9, 7,12, 7]
+#element =  0   1  2  3  4  5  6
+player_2 = [7, 6, 9, 5, 2, 8, 11]
 
-for i in range(len(player_1)):
+
+for i in range(len(player_1)):  
+    if (player_1[i] > player_2[i]):
+            print('Player_1 Wins the round',player_1[i],'with beating',player_2[i])
+
+    elif (player_2[i] > player_1[i]):
+            print('Player 2 wins the round',player_2[i],'with beating',player_1[i])
     
-    for x in c1:
-        if(c1 >= c2):
-            print('Player 1 WINS')
-        else:
-            (c1 == c2)
-            print('The round has draw')
+    else: (len(player_1) == len(player_2))
+    #print('This round has ended in a draw',)
 
-            for y in c2:
-                if (c2 >= c1):
-                    print('Player 2 WINS')
-                else:
-                     (c1 == c2)
-                print('The round has dra')
+        ########### Result ###########
+                        
+print('Player_1 the game, by',i - 2,'wins to',i-4)
 
 
-
-
-
-
-########### Result ###########
-#print('player_1 wins the game, by 4 wins to 2'.format(player_1, player_2));
---
+++ Traning/Rascunho.py
@@ -0,0 +1,2 @@
+(player_2[i] > player_1[i]):
+            print('PLayer 2 wins the round',player_2[i],'with beating',player_1[i])
