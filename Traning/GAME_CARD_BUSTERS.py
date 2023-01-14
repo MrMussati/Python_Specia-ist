fc38277a31f62bf9e56658e9b6d2bd8ccd33db0f
MrMussati <bruno.cesar.rocha12@gmail.com>
2023-01-14 04:12:18 +0000

    python

--- Traning/GAME_CARD_BUSTERS.py
+++ Traning/GAME_CARD_BUSTERS.py
@@ -6,30 +6,37 @@ player_1 = [10, 6, 8, 9, 7,12, 7]
 player_2 = [7, 6, 9, 5, 2, 8, 11]
 
 
-p1_wins = [0]
-p2_wins = [0]
+p1_wins = 0
+p2_wins = 0
 
 for i in range(len(player_1)):  
     if (player_1[i] > player_2[i]):
+        p1_wins += 1
         print('Player_1 Wins the round',player_1[i],'with beating',player_2[i])
 
     elif (player_1[i]) == (player_2[i]):
         print('This round has ended in a draw')
         
-        (player_2[i] > player_1[i])
+    else : 
+
+        p2_wins += 1
         print('Player 2 wins the round',player_2[i],'with beating',player_1[i])
     
     
- 
 ################## restult #####################
 
-for x in p1_wins:
-        if (p1_wins > p2_wins):
-                print('Player 1 wins The game,by ',p1_wins,'wins to ',p2_wins)
-        
-        elif (p1_wins )== (p2_wins):
-                #print('the game has ended un a draw')
+for x in p1_wins,p2_wins:
 
-                (p2_wins < p1_wins)
-                print('Player 2 wins the game,by',p1_wins,'wins to',p2_wins)
+    if p2_wins > p1_wins:
+         
+         print('Player 2 wins The game,by ',p2_wins ,'wins to ',p1_wins)
 
+    elif p1_wins == p2_wins:
+            print('the game has ended un a draw')
+
+    else:
+        p2_wins > p1_wins
+          
+else:
+    
+    print('*Player 1* wins The game,by',p1_wins ,'wins to',p2_wins)
