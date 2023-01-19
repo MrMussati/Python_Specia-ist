cf30ba42d5e964507d6b04703c68625c7681bdd5
MrMussati <bruno.cesar.rocha12@gmail.com>
2023-01-19 04:30:28 +0000

    python

---
+++ loan.py
@@ -0,0 +1,30 @@
+####### LOAN ########
+
+
+kitty = 500
+
+request = []
+#request = [60,20,100,40,300,200,100]
+
+##### MODO ####
+
+loan = open ('C:\\Users\\bruno\Desktop\\VSCODE\\IT_Specialist_python\\Ecollege\\loan_requests.txt','r') 
+for i in loan:
+   
+    if i in loan :
+        loan.write('request of',i,'paid in full'+'\n ')
+        print(loan.read())
+    print(i,'was paid'+'\n ')
+    
+    
+
+
+   
+    
+
+        
+
+
+
+    
+    
